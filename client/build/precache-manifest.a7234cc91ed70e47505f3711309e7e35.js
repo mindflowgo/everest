95e238542b038b93b5b0afc24e6bc4a93d9a286f
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-04-11 03:44:40 -0400

    Updated react build files for Heroku

---
+++ client/build/precache-manifest.a7234cc91ed70e47505f3711309e7e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f750f64036d3cff29259d8190de0919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "281a4441042aea5055e4",
+    "url": "/static/css/main.e71a1f96.chunk.css"
+  },
+  {
+    "revision": "05c6a7ea7ab59d20b573",
+    "url": "/static/js/2.135bb021.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.135bb021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281a4441042aea5055e4",
+    "url": "/static/js/main.06319322.chunk.js"
+  },
+  {
+    "revision": "b0f15bf39396454d8f9d",
+    "url": "/static/js/runtime-main.fd576518.js"
+  }
+]);
